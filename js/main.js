cc974741f57c494fbf5d93a19ab155ad4e9632f1
kswap-app <132029706+kswap-app@users.noreply.github.com>
2023-05-19 17:22:09 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -62,65 +62,95 @@ $(window).bind("load", function () {
     async function addHiveNodes() {
         try 
         {
+            var buttonHive = document.getElementById("popup-button-hive");
+            var popupHive = document.getElementById("popup-container-hive");
             const tableBody = document.querySelector("#api-list-hive tbody");
             const workingNodes = [];
             const failedNodes = [];
-        
+    
             for (let i = 0; i < rpc_nodes.length; i++) 
             {
                 const nodeUrl = rpc_nodes[i];
                 const row = document.createElement("tr");
                 const urlCell = document.createElement("td");
                 const statusCell = document.createElement("td");
-        
+    
                 urlCell.textContent = nodeUrl;
                 urlCell.classList.add("node-url"); // add new class to url cell
                 statusCell.textContent = "Checking...";
-        
+    
                 row.appendChild(urlCell);
                 row.appendChild(statusCell);
-        
+    
                 tableBody.appendChild(row);
-        
+    
                 // Check node status
                 checkHiveNodeStatus(nodeUrl, statusCell);
-        
+    
                 // Check node status every minute
                 setInterval(() => checkHiveNodeStatus(nodeUrl, statusCell), 60 * 1000);
             }
-      
+    
             // Reorder the list of nodes based on their status
             setTimeout(() => {
                 const rows = Array.from(tableBody.getElementsByTagName("tr"));
-        
+    
                 rows.forEach((row) => {
-                    if (row.lastChild.textContent === "Working") 
-                    {
+                    if (row.lastChild.textContent === "Working") {
                         workingNodes.push(row);
-                    } 
-                    else 
-                    {
+                    } else {
                         failedNodes.push(row);
                     }
                 });
-        
+    
                 tableBody.innerHTML = "";
-        
+    
                 // Append workingNodes first, then failedNodes
                 workingNodes.forEach((row) => {
                     tableBody.appendChild(row);
                 });
-        
+    
                 failedNodes.forEach((row) => {
                     tableBody.appendChild(row);
                 });
             }, 5000);
+    
+            // Add event listeners to the rows in the table body
+            var rowsHive = tableBody.getElementsByTagName("tr");
+            for (var i = 0; i < rowsHive.length; i++) 
+            {
+                rowsHive[i].addEventListener("click", function (event) {
+                    // Prevent the default link behavior
+                    event.preventDefault();
+    
+                    // Get the node URL from the first cell in the row
+                    var nodeUrl = this.cells[0].textContent;
+    
+                    // Set the API endpoint to the selected node
+                    hive.api.setOptions({ url: nodeUrl });
+    
+                    // Update the button text
+                    buttonHive.value = nodeUrl;
+                    buttonHive.innerHTML = nodeUrl;
+    
+                    // Save the selected endpoint to local storage
+                    localStorage.setItem("selectedEndpoint", nodeUrl);
+    
+                    // Hide the popup
+                    popupHive.style.display = "none";
+    
+                    // Reload the page after 1 second (adjust the time as needed)
+                    setTimeout(function () {
+                        location.reload();
+                    }, 1000);
+                });
+            }
         } 
         catch (error) 
         {
             console.log("Error at addHiveNodes(): ", error);
         }
-    };    
+    };       
 
     async function checkEngineNodeStatus(nodeUrl, statusElement) {
         try 
@@ -150,65 +180,95 @@ $(window).bind("load", function () {
     async function addEngineNodes() {
         try 
         {
+            var buttonEngine = document.getElementById("popup-button-engine");
+            var popupEngine = document.getElementById("popup-container-engine");
             const tableBody = document.querySelector("#api-list-engine tbody");
             const workingNodes = [];
             const failedNodes = [];
-        
+    
             for (let i = 0; i < he_rpc_nodes.length; i++) 
             {
                 const nodeUrl = he_rpc_nodes[i];
                 const row = document.createElement("tr");
                 const urlCell = document.createElement("td");
                 const statusCell = document.createElement("td");
-        
+    
                 urlCell.textContent = nodeUrl;
                 urlCell.classList.add("node-url"); // add new class to url cell
                 statusCell.textContent = "Checking...";
-        
+    
                 row.appendChild(urlCell);
                 row.appendChild(statusCell);
-        
+    
                 tableBody.appendChild(row);
-        
+    
                 // Check node status
                 checkEngineNodeStatus(nodeUrl, statusCell);
-        
+    
                 // Check node status every minute
                 setInterval(() => checkEngineNodeStatus(nodeUrl, statusCell), 60 * 1000);
             }
-      
+    
             // Reorder the list of nodes based on their status
             setTimeout(() => {
                 const rows = Array.from(tableBody.getElementsByTagName("tr"));
-        
+    
                 rows.forEach((row) => {
-                    if (row.lastChild.textContent === "Working") 
-                    {
+                    if (row.lastChild.textContent === "Working") {
                         workingNodes.push(row);
-                    } 
-                    else 
-                    {
+                    } else {
                         failedNodes.push(row);
                     }
                 });
-        
+    
                 tableBody.innerHTML = "";
-        
+    
                 // Append workingNodes first, then failedNodes
                 workingNodes.forEach((row) => {
                     tableBody.appendChild(row);
                 });
-        
+    
                 failedNodes.forEach((row) => {
                     tableBody.appendChild(row);
                 });
             }, 5000);
+    
+            // Add event listeners to the rows in the table body
+            var rowsEngine = tableBody.getElementsByTagName("tr");
+            for (var i = 0; i < rowsEngine.length; i++) 
+            {
+                rowsEngine[i].addEventListener("click", function (event) {
+                    // Prevent the default link behavior
+                    event.preventDefault();
+    
+                    // Get the node URL from the first cell in the row
+                    var nodeUrl = this.cells[0].textContent;
+    
+                    // Set the API endpoint to the selected node
+                    ssc = new SSC(nodeUrl);
+    
+                    // Update the button text
+                    buttonEngine.value = nodeUrl;
+                    buttonEngine.innerHTML = nodeUrl;
+    
+                    // Save the selected endpoint to local storage
+                    localStorage.setItem("selectedEngEndpoint", nodeUrl);
+    
+                    // Hide the popup
+                    popupEngine.style.display = "none";
+    
+                    // Reload the page after 1 second (adjust the time as needed)
+                    setTimeout(function () {
+                        location.reload();
+                    }, 1000);
+                });
+            }
         } 
         catch (error) 
         {
             console.log("Error at addEngineNodes(): ", error);
         }
-    };  
+    };      
     
     async function initializeHiveAPI() {
         var selectedEndpoint = await getSelectedEndpoint();
@@ -231,9 +291,8 @@ $(window).bind("load", function () {
     }
 
     async function processAPIs() {
-        try {
-            await addHiveNodes();
-            await addEngineNodes();
+        try 
+        {              
             await initializeHiveAPI();
             await initializeEngineAPI();            
         } 
@@ -255,114 +314,126 @@ $(window).bind("load", function () {
         return Math.floor(val * 1000) / 1000;
     };
 
-    $(document).ready(function() {
-        // Get a reference to the button and the popup container
-        var buttonHive = document.getElementById("popup-button-hive");
-        var popupHive = document.getElementById("popup-container-hive");        
+    $(document).ready(function() {        
+        loadHiveNode();
+        loadEngineNode();               
+    });
 
-        // Add an event listener to the button
-        buttonHive.addEventListener("click", function() {
-            // Show the popup
-            popupHive.style.display = "block";
-        });
-
-        // Get a reference to the API list table body
-        var tableBodyHive = document.querySelector("#api-list-hive tbody");
-
-        // Add event listeners to the rows in the table body
-        var rowsHive = tableBodyHive.getElementsByTagName("tr");
-        for (var i = 0; i < rowsHive.length; i++) 
+    async function loadHiveNode() {
+        try 
         {
-            rowsHive[i].addEventListener("click", function(event) {
-                // Prevent the default link behavior
-                event.preventDefault();
-
-                // Get the node URL from the first cell in the row
-                var nodeUrl = this.cells[0].textContent;
-
-                // Set the API endpoint to the selected node
-                hive.api.setOptions({ url: nodeUrl });
-
-                // Update the button text
-                buttonHive.value = nodeUrl;
-                buttonHive.innerHTML = nodeUrl;
-
-                // Save the selected endpoint to local storage
-                localStorage.setItem("selectedEndpoint", nodeUrl);
-
+            // Get a reference to the button and the popup container
+            var buttonHive = document.getElementById("popup-button-hive");
+            var popupHive = document.getElementById("popup-container-hive");
+        
+            // Add an event listener to the button
+            buttonHive.addEventListener("click", function () {
+                // Show the popup
+                popupHive.style.display = "block";
+                addHiveNodes();
+            });
+    
+            // Add an event listener to the close button
+            var closeButtonHive = document.getElementById("close-button-hive");
+            closeButtonHive.addEventListener("click", function () {
                 // Hide the popup
                 popupHive.style.display = "none";
-
-                // Reload the page after 1 second (adjust the time as needed)
-                setTimeout(function() {
-                    location.reload();
-                }, 1000);
             });
-        }
-
-        // Add an event listener to the close button
-        var closeButtonHive = document.getElementById("close-button-hive");
-        closeButtonHive.addEventListener("click", function() {
-            // Hide the popup
-            popupHive.style.display = "none";
-        });
-        
-        /*
-        *   HE RPC NODES
-        */
-
-
-        // Get a reference to the button and the popup container
-        var buttonEngine = document.getElementById("popup-button-engine");
-        var popupEngine = document.getElementById("popup-container-engine");        
-
-        // Add an event listener to the button
-        buttonEngine.addEventListener("click", function() {
-            // Show the popup
-            popupEngine.style.display = "block";
-        });
-
-        // Get a reference to the API list table body
-        var tableBodyEngine = document.querySelector("#api-list-engine tbody");
-
-        // Add event listeners to the rows in the table body
-        var rowsEngine = tableBodyEngine.getElementsByTagName("tr");
-        for (var i = 0; i < rowsEngine.length; i++) 
+    
+            // Get a reference to the API list table body
+            var tableBodyHive = document.querySelector("#api-list-hive tbody");
+    
+            // Add an event listener to the table body
+            tableBodyHive.addEventListener("click", function (event) {
+                var target = event.target;
+                if (target && target.nodeName === "TD") 
+                {
+                    // Get the node URL from the first cell in the row
+                    var nodeUrl = target.parentNode.cells[0].textContent;
+    
+                    // Set the API endpoint to the selected node
+                    hive.api.setOptions({ url: nodeUrl });
+    
+                    // Update the button text
+                    buttonHive.value = nodeUrl;
+                    buttonHive.innerHTML = nodeUrl;
+    
+                    // Save the selected endpoint to local storage
+                    localStorage.setItem("selectedEndpoint", nodeUrl);
+    
+                    // Hide the popup
+                    popupHive.style.display = "none";
+    
+                    // Reload the page after 1 second (adjust the time as needed)
+                    setTimeout(function () {
+                        location.reload();
+                    }, 1000);
+                }
+            });
+        } 
+        catch (error) 
         {
-            rowsEngine[i].addEventListener("click", function(event) {
-                // Prevent the default link behavior
-                event.preventDefault();
-
-                // Get the node URL from the first cell in the row
-                var nodeUrl = this.cells[0].textContent;
-
-                // Set the API endpoint to the selected node
-                ssc = new SSC(nodeUrl);
-
-                // Update the button text
-                buttonEngine.value = nodeUrl;
-                buttonEngine.innerHTML = nodeUrl;
-
-                // Save the selected endpoint to local storage
-                localStorage.setItem("selectedEngEndpoint", nodeUrl);
-
+            console.log("Error at loadHiveNode(): ", error);
+        }
+    }; 
+    
+    async function loadEngineNode() {
+        try 
+        {
+            // Get a reference to the button and the popup container
+            var buttonEngine = document.getElementById("popup-button-engine");
+            var popupEngine = document.getElementById("popup-container-engine");
+        
+            // Add an event listener to the button
+            buttonEngine.addEventListener("click", function () {
+                // Show the popup
+                popupEngine.style.display = "block";
+                addEngineNodes();
+            });
+    
+            // Add an event listener to the close button
+            var closeButtonEngine = document.getElementById("close-button-engine");
+            closeButtonEngine.addEventListener("click", function () {
                 // Hide the popup
                 popupEngine.style.display = "none";
-
-                // Reload the page after 1 second (adjust the time as needed)
-                setTimeout(function() {
-                    location.reload();
-                }, 1000);
             });
+    
+            // Get a reference to the API list table body
+            var tableBodyEngine = document.querySelector("#api-list-engine tbody");
+    
+            // Add an event listener to the table body
+            tableBodyEngine.addEventListener("click", function (event) {
+                var target = event.target;
+                if (target && target.nodeName === "TD") 
+                {
+                    // Get the node URL from the first cell in the row
+                    var nodeUrl = target.parentNode.cells[0].textContent;
+    
+                    // Set the API endpoint to the selected node
+                    ssc = new SSC(nodeUrl);
+    
+                    // Update the button text
+                    buttonEngine.value = nodeUrl;
+                    buttonEngine.innerHTML = nodeUrl;
+    
+                    // Save the selected endpoint to local storage
+                    localStorage.setItem("selectedEngEndpoint", nodeUrl);
+    
+                    // Hide the popup
+                    popupEngine.style.display = "none";
+    
+                    // Reload the page after 1 second (adjust the time as needed)
+                    setTimeout(function () {
+                        location.reload();
+                    }, 1000);
+                }
+            });
+        } 
+        catch (error) 
+        {
+            console.log("Error at loadEngineNode(): ", error);
         }
-
-        // Add an event listener to the close button
-        var closeButtonEngine = document.getElementById("close-button-engine");
-        closeButtonEngine.addEventListener("click", function() {
-            // Hide the popup
-            popupEngine.style.display = "none";
-        });        
-    });
+    };  
 
     async function getBalances(account) {
         try
